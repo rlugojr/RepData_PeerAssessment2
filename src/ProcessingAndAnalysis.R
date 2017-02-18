fc56879c4b4d2264a7269721ae5c78f0ab638a86
Ray Lugo, Jr <rlugojr@gmail.com>
2017-02-18 01:17:14 -0500

    Updated Answer 1
    Cleaned up code by limiting it to answering question 1 and removing all
    other analysis.
    Formatted multi panel plot.

--- src/ProcessingAndAnalysis.R
+++ src/ProcessingAndAnalysis.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(reshape2)
 library(data.table)
+library(stringi)
 
 url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 csvStormData <- paste0(getwd(),"../../repdata_data_StormData.csv")
@@ -18,53 +19,23 @@ stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "FLASH FLOODS", regex = "(.*)
 stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "EXTREME HEAT", regex = "(.*)HEAT(.*)")
 stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "TORNADO", regex = "(.*)TORNADO(.*)")
 
-
-Events_with_Casualties_by_Year <- stormData %>%
-    select(YEAR, EVTYPE, FATALITIES, INJURIES) %>%
-    filter(FATALITIES > 0 | INJURIES > 0) %>%
-    group_by(YEAR, EVTYPE) %>%
-    summarise(Events = n(),totalFatalities = sum(FATALITIES), totalInjuries = sum(INJURIES), totalCasualties = sum(FATALITIES + INJURIES))
-
-
-Events_with_Casualty_Total <- Events_with_Casualties_by_Year %>% group_by(EVTYPE) %>% summarise(Events = n(), totalCasualties = sum(totalCasualties)) %>% arrange(desc(totalCasualties))
-
-Events_with_Casualty_Total #Note Thunderstorm winds is in the top 20 3 times, textual variation, so the EVTYPE would require cleanup for a more precise report
-
-top_10_EVTYPE <- data.frame( EVTYPE = Events_with_Casualty_Total[1:10,]$EVTYPE, stringsAsFactors = F) #added two more because of the 2 extra variations of "THUNDERSTORM WINDS"
-
-top_10_EVTYPE
-
-top_10_EVTYPE_OVER_TIME <- inner_join(Events_with_Casualties_by_Year, top_10_EVTYPE, by = "EVTYPE")
-
-ggplot(top_10_EVTYPE_OVER_TIME, aes(x = YEAR, y = totalCasualties, fill = EVTYPE)) +
-    geom_line(aes(color = EVTYPE))
-
-ggplot(filter(top_10_EVTYPE_OVER_TIME, YEAR >= 1993), aes(x = YEAR, y = totalCasualties, fill = EVTYPE)) +
-    geom_line(aes(color = EVTYPE))
-
-Events_with_Casualty_Total_1993 <- Events_with_Casualties_by_Year %>% filter(YEAR >= 1993) %>% group_by(EVTYPE) %>% summarise(Events = n(), totalCasualties = sum(totalCasualties)) %>% arrange(desc(totalCasualties))
-
-top_5_EVTYPE_1993 <- data.frame( EVTYPE = Events_with_Casualty_Total_1993[1:5,]$EVTYPE, stringsAsFactors = F)
-
-top_5_EVTYPE_1993_OVER_TIME <- inner_join(filter(Events_with_Casualties_by_Year, YEAR >= 1993), top_5_EVTYPE_1993, by = "EVTYPE")
-
-
-ggplot(top_5_EVTYPE_1993_OVER_TIME, aes(x = YEAR, y = totalCasualties, fill = EVTYPE)) +
-    geom_line(aes(color = EVTYPE))
-
-health_top_5_over_time <- top_5_EVTYPE_1993_OVER_TIME %>%
-    select(EVTYPE, totalFatalities, totalInjuries)  %>%
+population_health_1993 <- stormData %>%
+    filter(YEAR >= 1993 & (FATALITIES != 0 & INJURIES != 0)) %>%
+    select(EVTYPE, FATALITIES, INJURIES) %>%
     group_by(EVTYPE) %>%
-    summarize(totalFatalities = sum(totalFatalities), totalInjuries = sum(totalInjuries), totalCasualties = sum(totalFatalities + totalInjuries)) %>%
-    melt(id.vars = "EVTYPE")
+    summarize(FATALITIES = sum(FATALITIES), INJURIES = sum(INJURIES), CASUALTIES = sum(FATALITIES + INJURIES)) %>%
+    arrange(desc(CASUALTIES))
 
-bar_population_health_Top_5 <- ggplot(health_top_5_over_time, aes(x = reorder(EVTYPE, value), y = value, fill = variable, label = value)) +
-    geom_bar(stat = "identity", position = "stack") +
+top_5_events <- melt(population_health_1993[1:5,],id.vars = "EVTYPE")
+levels(top_5_events$EVTYPE) <- population_health_1993[1:5,]$EVTYPE
+
+bar_population_health_Top_5 <- ggplot(top_5_events, aes(x = reorder(EVTYPE, value), y = value, fill = variable, label = value)) +
+    geom_bar(stat = "identity") +
     geom_text(size = 3, color = "white", position = position_stack(vjust = 0.5)) +
-    #scale_y_log10() +
-    coord_flip() +
-    facet_grid(variable~.) +
-    scale_fill_manual(values = c("firebrick","steelblue","black"), name = "Outcome", labels = c("Fatalities", "Injuries", "Casualties")) +
-    labs(Title = "Top 5 Events with Highest Casualties",x = "Event Type", y = "Casualties (log 10)")
+    scale_y_sqrt() +
+    facet_grid(.~variable) +
+    scale_fill_manual(values = c("firebrick","steelblue","black"), labels = c("Fatalities", "Injuries", "Casualties")) +
+    theme(axis.text.x = element_text(angle = 45, size = 6, hjust = 1)) +
+    labs(main = "Top 5 Events with Highest Casualties 1993 - 2011", x = "Event Type", y = "Total Casualties (Log Sqrt)")
 
 bar_population_health_Top_5

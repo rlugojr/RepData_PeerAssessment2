30654b7f4958234e69c740e28a1349dc73a859af
Ray Lugo, Jr <rlugojr@gmail.com>
2017-02-18 04:08:39 -0500

    Answer 2 and  updated charts
    Calculations and charts for answer 2.
    Updated chart formatting.

--- src/ProcessingAndAnalysis.R
+++ src/ProcessingAndAnalysis.R
@@ -21,7 +21,7 @@ if (!file.exists(csvStormData)) {
 }
 
 # data File processing
-keepCols <- c("BGN_DATE","STATE","EVTYPE","FATALITIES","INJURIES","PROPDMG", "PROPDMGEXP", "CROPDMG", "CROPDMGEXP")
+keepCols <- c("BGN_DATE","EVTYPE","FATALITIES","INJURIES","PROPDMG", "PROPDMGEXP", "CROPDMG", "CROPDMGEXP")
 
 stormData <- fread(file = csvStormData, sep = ",", header = T, select = keepCols, na.strings = "NA", verbose = T, autostart = T,
                    strip.white = T, data.table = T, quote = '\"', stringsAsFactors = F, showProgress = T)
@@ -32,10 +32,14 @@ stormData$BGN_DATE <- as.Date(stormData$BGN_DATE,"%m/%d/%Y")
 stormData$YEAR <- year(stormData$BGN_DATE)
 
 #cleanup pertinent EVTYPE variants using regexes before analysis for more accuracy
-stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "THUNDERSTORM WINDS", regex = "(.)*?(A-Z)?T\\w+\\sWIN(D)?(S)?\\D?(.*)?")
+stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "THUNDERSTORM", regex = "(.)*?(A-Z)?T\\w+\\sWIN(D)?(S)?\\D?(.*)?")
+stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "THUNDERSTORM", regex = "(.*)THUNDERSTORM(.*)")
 stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "FLASH FLOODS", regex = "(.*)?FLOOD(.*)?")
 stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "EXTREME HEAT", regex = "(.*)HEAT(.*)")
 stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "TORNADO", regex = "(.*)TORNADO(.*)")
+stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "HAIL", regex = "(.*)HAIL(.*)")
+stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "HURRICANE", regex = "(.*)HURRICANE(.*)")
+stormData$EVTYPE <- stri_replace(stormData$EVTYPE, "TROPICAL STORM", regex = "(.*)TROPICAL STORM(.*)")
 
 #The majority of data reporting for all EVTYPES starts around 1993.
 #Using that as a selection filter on YEAR to keep tornados from becoming an outlier.
@@ -48,22 +52,66 @@ population_health_1993 <- stormData %>%
     arrange(desc(CASUALTIES))
 
 #
-top_5_events <- melt(population_health_1993[1:5,],id.vars = "EVTYPE")
-levels(top_5_events$EVTYPE) <- population_health_1993[1:5,]$EVTYPE
+top_5_pop_events <- melt(population_health_1993[1:5,],id.vars = "EVTYPE")
+levels(top_5_pop_events$EVTYPE) <- population_health_1993[1:5,]$EVTYPE
 
-bar_population_health_Top_5 <- ggplot(top_5_events, aes(x = reorder(EVTYPE, value), y = value, fill = variable, label = value)) +
+bar_population_health_Top_5 <- ggplot(top_5_pop_events, aes(x = reorder(EVTYPE, value), y = value, fill = variable, label = round(value/1000,1))) +
     geom_bar(stat = "identity") +
     geom_text(size = 3, color = "white", position = position_stack(vjust = 0.5)) +
     scale_y_sqrt() +
     facet_grid(.~variable) +
     scale_fill_manual(values = c("firebrick","steelblue","black"), labels = c("Fatalities", "Injuries", "Casualties")) +
     theme(axis.text.x = element_text(angle = 45, size = 6, hjust = 1)) +
-    labs(main = "Top 5 Events with Highest Casualties 1993 - 2011", x = "Event Type", y = "Total Casualties (Log Sqrt)")
+    labs(main = "Top 5 Events with Highest Casualties 1993 - 2011", x = "Event", y = "Total Casualties in Thousands (Log sqrt)") +
+    ggsave("figures/top_5_pop_events.png")
 
 bar_population_health_Top_5
 
 
 #Question 2
+options(scipen = 999)
+#create tables to hold damage code and corresponding magnitude for PROP and CROP.
+propDamageCalc <- data.frame(
+    PROPDMGEXP = c("K", "M", "B"),
+    PROPDMGMULT = c(1000,1000000,1000000000),
+    stringsAsFactors = F
+)
+
+cropDamageCalc <- data.frame(
+    CROPDMGEXP = c("K", "M", "B"),
+    CROPDMGMULT = c(1000,1000000,1000000000),
+    stringsAsFactors = F
+)
+
+damageData <- stormData %>%
+    filter(YEAR >= 1993 & (PROPDMG != 0 | CROPDMG != 0)) %>%
+    select(EVTYPE, PROPDMG, PROPDMGEXP, CROPDMG, CROPDMGEXP)
+
+damageData <- inner_join(damageData, propDamageCalc, by = "PROPDMGEXP")
+damageData <- inner_join(damageData, cropDamageCalc, by = "CROPDMGEXP")
+
+damageData <- mutate(damageData, PROPDMGCOST = PROPDMG * PROPDMGMULT, CROPDMGCOST = CROPDMG * CROPDMGMULT)
+
+Event_Crop_Prop_Cost <- damageData %>%
+    select(EVTYPE, PROPDMGCOST, CROPDMGCOST) %>%
+    group_by(EVTYPE) %>%
+    summarize(PROPERTY = sum(PROPDMGCOST), CROP = sum(CROPDMGCOST), TOTAL = sum(PROPDMGCOST + CROPDMGCOST)) %>%
+    arrange(desc(TOTAL))
+
+top_5_cost_events <- melt(Event_Crop_Prop_Cost[1:5,],id.vars = "EVTYPE")
+levels(top_5_cost_events$EVTYPE) <- Event_Crop_Prop_Cost[1:5,]$EVTYPE
+
+bar_Cost_Top_5 <- ggplot(top_5_cost_events, aes(x = reorder(EVTYPE, value), y = value, fill = variable, label = round(value/1000000000,1))) +
+    geom_bar(stat = "identity") +
+    geom_text(size = 3, color = "white", position = position_stack(vjust = 0.5)) +
+    scale_y_sqrt() +
+    facet_grid(.~variable) +
+    scale_fill_manual(values = c("darkgreen","brown","black"), labels = c("Property", "Crop", "Total")) +
+    theme(axis.text.x = element_text(angle = 45, size = 6, hjust = 1)) +
+    labs(main = "Top 5 Events with Highest Costs 1993 - 2011", x = "Event", y = "Cost in Billions (Log sqrt)") +
+    ggsave("figures/top_5_cost_events.png")
+
+bar_Cost_Top_5
 
 
-
+options(scipen = 0)

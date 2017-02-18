4ba67b2f77be652c17423fa4664f4ff77fb15fee
Ray Lugo, Jr <rlugojr@gmail.com>
2017-02-18 15:08:04 -0500

    Chart Updates and Scratchpad script
    Additional chart formatting chances.  Adding commas as big.marks for
    large numbers.
    Saving a copy of the scratchpad R file used for analysis.

--- src/ProcessingAndAnalysis.R
+++ src/ProcessingAndAnalysis.R
@@ -55,7 +55,7 @@ population_health_1993 <- stormData %>%
 top_5_pop_events <- melt(population_health_1993[1:5,],id.vars = "EVTYPE")
 levels(top_5_pop_events$EVTYPE) <- population_health_1993[1:5,]$EVTYPE
 
-bar_population_health_Top_5 <- ggplot(top_5_pop_events, aes(x = reorder(EVTYPE, value), y = value, fill = variable, label = round(value/1000,1))) +
+bar_population_health_Top_5 <- ggplot(top_5_pop_events, aes(x = reorder(EVTYPE, value), y = value, fill = variable, label = format(round(value/1000,1), big.mark = ",", scientific = F))) +
     geom_bar(stat = "identity") +
     geom_text(size = 3, color = "white", position = position_stack(vjust = 0.5)) +
     scale_y_sqrt() +
@@ -101,14 +101,14 @@ Event_Crop_Prop_Cost <- damageData %>%
 top_5_cost_events <- melt(Event_Crop_Prop_Cost[1:5,],id.vars = "EVTYPE")
 levels(top_5_cost_events$EVTYPE) <- Event_Crop_Prop_Cost[1:5,]$EVTYPE
 
-bar_Cost_Top_5 <- ggplot(top_5_cost_events, aes(x = reorder(EVTYPE, value), y = value, fill = variable, label = round(value/1000000000,1))) +
+bar_Cost_Top_5 <- ggplot(top_5_cost_events, aes(x = reorder(EVTYPE, value), y = value, fill = variable, label = format(round(value/1000000000,1), big.mark = ",", scientific = F))) +
     geom_bar(stat = "identity") +
     geom_text(size = 3, color = "white", position = position_stack(vjust = 0.5)) +
     scale_y_sqrt() +
     facet_grid(.~variable) +
     scale_fill_manual(values = c("darkgreen","brown","black"), labels = c("Property", "Crop", "Total")) +
     theme(axis.text.x = element_text(angle = 45, size = 6, hjust = 1)) +
-    labs(main = "Top 5 Events with Highest Costs 1993 - 2011", x = "Event", y = "Cost in Billions (Log sqrt)") +
+    labs(title = "Top 5 Events with Highest Costs 1993 - 2011", x = "Event", y = "Cost in Billions (Log sqrt)") +
     ggsave("figures/top_5_cost_events.png")
 
 bar_Cost_Top_5
